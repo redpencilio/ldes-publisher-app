5f17ac267bba0dae343a2b3b8898a3860732b8a6
Jonathan Poelman <jonathan.poelman@protonmail.com>
2022-04-26 09:54:44 +0200

    initial version of publisher application which publishes posts to ldes stream

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,79 +1,103 @@
 export default [
-    {
-        match: {
-            predicate: {
-                type: "uri",
-                value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
-            },
-            object: {
-                type: "uri",
-                value: "https://schema.org/SocialMediaPosting",
-            },
-        },
-        callback: {
-            url: "http://socialplatformwsmicroservice/posts/", method: "POST"
-        },
-        options: {
-            resourceFormat: "v0.0.1",
-            gracePeriod: 1000,
-            ignoreFromSelf: true
-        }
-    },
-    {
-        match: {
-            predicate: {
-                type: "uri",
-                value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
-            },
-            object: {
-                type: "uri",
-                value: "https://schema.org/SocialMediaPosting",
-            },
-        },
-        callback: {
-            url: "http://socialplatformmicroservice/posts/", method: "POST"
-        },
-        options: {
-            resourceFormat: "v0.0.1",
-            gracePeriod: 1000,
-            ignoreFromSelf: true
-        }
-    },
-    {
-        match: {
-            predicate: {
-                type: "uri",
-                value: "https://schema.org/author",
-            },
-        },
-        callback: {
-            url: "http://socialplatformmicroservice/posts/", method: "POST"
-        },
-        options: {
-            resourceFormat: "v0.0.1",
-            gracePeriod: 1000,
-            ignoreFromSelf: true
-        }
-    },
-    {
-        match: {
-            predicate: {
-                type: "uri",
-                value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
-            },
-            object: {
-                type: "uri",
-                value: "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject",
-            },
-        },
-        callback: {
-            url: "http://socialplatformmicroservice/posts/", method: "POST"
-        },
-        options: {
-            resourceFormat: "v0.0.1",
-            gracePeriod: 1000,
-            ignoreFromSelf: true
-        }
-    },
-    
-]
+	{
+		match: {
+			predicate: {
+				type: "uri",
+				value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+			},
+			object: {
+				type: "uri",
+				value: "https://schema.org/SocialMediaPosting",
+			},
+		},
+		callback: {
+			url: "http://socialplatformwsmicroservice/posts/",
+			method: "POST",
+		},
+		options: {
+			resourceFormat: "v0.0.1",
+			gracePeriod: 1000,
+			ignoreFromSelf: true,
+		},
+	},
+	{
+		match: {
+			predicate: {
+				type: "uri",
+				value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+			},
+			object: {
+				type: "uri",
+				value: "https://schema.org/SocialMediaPosting",
+			},
+		},
+		callback: {
+			url: "http://socialplatformmicroservice/posts-ldes/",
+			method: "POST",
+		},
+		options: {
+			resourceFormat: "v0.0.1",
+			gracePeriod: 1000,
+			ignoreFromSelf: true,
+		},
+	},
+	{
+		match: {
+			predicate: {
+				type: "uri",
+				value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+			},
+			object: {
+				type: "uri",
+				value: "https://schema.org/SocialMediaPosting",
+			},
+		},
+		callback: {
+			url: "http://socialplatformmicroservice/posts/",
+			method: "POST",
+		},
+		options: {
+			resourceFormat: "v0.0.1",
+			gracePeriod: 1000,
+			ignoreFromSelf: true,
+		},
+	},
+	{
+		match: {
+			predicate: {
+				type: "uri",
+				value: "https://schema.org/author",
+			},
+		},
+		callback: {
+			url: "http://socialplatformmicroservice/posts/",
+			method: "POST",
+		},
+		options: {
+			resourceFormat: "v0.0.1",
+			gracePeriod: 1000,
+			ignoreFromSelf: true,
+		},
+	},
+	{
+		match: {
+			predicate: {
+				type: "uri",
+				value: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+			},
+			object: {
+				type: "uri",
+				value: "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject",
+			},
+		},
+		callback: {
+			url: "http://socialplatformmicroservice/posts/",
+			method: "POST",
+		},
+		options: {
+			resourceFormat: "v0.0.1",
+			gracePeriod: 1000,
+			ignoreFromSelf: true,
+		},
+	},
+];
--- socialplatform-microservice/app.js
+++ socialplatform-microservice/app.js
@@ -1,79 +1,119 @@
-import { app, query, update, sparql, errorHandler } from 'mu';
-import bodyParser from 'body-parser';
+import { app, query, update, sparql, errorHandler } from "mu";
+import bodyParser from "body-parser";
+import fetch from "node-fetch";
 
-const APPLICATION_GRAPH = 'http://mu.semte.ch/graphs/public';
-app.use(bodyParser.json({
-    limit: '500mb',
-    type: function(req) {
-      return /^application\/json/.test(req.get('content-type'));
-    },
-  }));
+const APPLICATION_GRAPH = "http://mu.semte.ch/graphs/public";
+app.use(
+	bodyParser.json({
+		limit: "500mb",
+		type: function (req) {
+			return /^application\/json/.test(req.get("content-type"));
+		},
+	})
+);
 
-
-function formatValue(tripleElement){
-    if (tripleElement.type == "uri"){
-        return `<${tripleElement.value}>`
-    } else {
-        return `"${tripleElement.value}"`;
-    }
+function formatValue(tripleElement) {
+	if (tripleElement.type == "uri") {
+		return `<${tripleElement.value}>`;
+	} else {
+		return `"${tripleElement.value}"`;
+	}
 }
 
-function toString(triple){
-    return `${formatValue(triple.subject)} ${formatValue(triple.predicate)} ${formatValue(triple.object)}.`;
+function toString(triple) {
+	return `${formatValue(triple.subject)} ${formatValue(
+		triple.predicate
+	)} ${formatValue(triple.object)}.`;
 }
 
-function generateQuery(inserts, deletes){
-    let insertString = ""
-    inserts.forEach(insert => {
-        insertString += toString(insert) + "\n";
-    })
-    let deleteString = "";
-    deletes.forEach(deleteOp => {
-        deleteString += toString(deleteOp) + "\n";
-    })
+function generateQuery(inserts, deletes) {
+	let insertString = "";
+	inserts.forEach((insert) => {
+		insertString += toString(insert) + "\n";
+	});
+	let deleteString = "";
+	deletes.forEach((deleteOp) => {
+		deleteString += toString(deleteOp) + "\n";
+	});
 
-    if(insertString){
-        insertString = `INSERT {
+	if (insertString) {
+		insertString = `INSERT {
             GRAPH <${APPLICATION_GRAPH}> {
                 ${insertString}
             }
         }`;
-    }
-    if(deleteString){
-        deleteString = `
+	}
+	if (deleteString) {
+		deleteString = `
         DELETE {
             GRAPH <${APPLICATION_GRAPH}> {
                 ${deleteString}
             }
-        }`
-    }
+        }`;
+	}
 
-    let queryStr = `
+	let queryStr = `
         ${deleteString}\n
         ${insertString}
     `;
-    return queryStr;
+	return queryStr;
 }
 
-app.post('/posts', function(req, res) {
-    let inserts = [];
-    let deletes = [];
-    req.body.forEach(operation => {
-        operation.inserts.forEach(insert => {
-            inserts.push(insert);
-            
-        });
-        operation.deletes.forEach(deleteOp => {
-            deletes.push(deleteOp);
-        })
-    });
+app.post("/posts", function (req, res) {
+	let inserts = [];
+	let deletes = [];
+	req.body.forEach((operation) => {
+		operation.inserts.forEach((insert) => {
+			inserts.push(insert);
+		});
+		operation.deletes.forEach((deleteOp) => {
+			deletes.push(deleteOp);
+		});
+	});
 
-    query(generateQuery(inserts, deletes)).then( function(response) {
-        res.send( JSON.stringify( response ) );
-      })
-      .catch( function(err) {
-        res.send( "Oops something went wrong: " + JSON.stringify( err ) );
-      });
-})
+	query(generateQuery(inserts, deletes))
+		.then(function (response) {
+			res.send(JSON.stringify(response));
+		})
+		.catch(function (err) {
+			res.send("Oops something went wrong: " + JSON.stringify(err));
+		});
+});
+
+async function sendLDESRequest(uri, body) {
+	const queryParams = new URLSearchParams({
+		resource: uri,
+		stream: "http://example.org/post-stream",
+		"relation-path": "https://schema.org/dateCreated",
+		fragmenter: "time-fragmenter",
+	});
+
+	return fetch(
+		"http://ldes-time-fragmenter:3000/social-media-posts?" + queryParams,
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "text/turtle",
+			},
+			body: body,
+		}
+	);
+}
+
+app.post("/posts-ldes", async function (req, res) {
+	console.log("ldes");
+	for (const operation of req.body) {
+		let resource = operation.inserts;
+		if (resource) {
+			let subject = resource[0].subject.value;
+			let turtleBody = "";
+			resource.forEach((triple) => {
+				turtleBody += toString(triple) + "\n";
+			});
+			let response = await sendLDESRequest(subject, turtleBody);
+			console.log(response);
+		}
+	}
+});
 
 app.use(errorHandler);
