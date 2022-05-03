7472157338a90f0dec96d2f4d8026b4a0331584f
Jonathan Poelman <jonathan.poelman@protonmail.com>
2022-05-03 15:21:51 +0200

    update to consumer submodule

--- socialplatform-microservice/app.js
+++ socialplatform-microservice/app.js
@@ -1,79 +1,82 @@
-import { app, query, update, sparql, errorHandler } from 'mu';
-import bodyParser from 'body-parser';
+import { app, query, update, sparql, errorHandler } from "mu";
+import bodyParser from "body-parser";
 
-const APPLICATION_GRAPH = 'http://mu.semte.ch/graphs/public';
-app.use(bodyParser.json({
-    limit: '500mb',
-    type: function(req) {
-      return /^application\/json/.test(req.get('content-type'));
-    },
-  }));
+const APPLICATION_GRAPH = "http://mu.semte.ch/graphs/public";
+app.use(
+	bodyParser.json({
+		limit: "500mb",
+		type: function (req) {
+			return /^application\/json/.test(req.get("content-type"));
+		},
+	})
+);
 
-
-function formatValue(tripleElement){
-    if (tripleElement.type == "uri"){
-        return `<${tripleElement.value}>`
-    } else {
-        return `"${tripleElement.value}"`;
-    }
+function formatValue(tripleElement) {
+	if (tripleElement.type == "uri") {
+		return `<${tripleElement.value}>`;
+	} else {
+		return `"${tripleElement.value}"`;
+	}
 }
 
-function toString(triple){
-    return `${formatValue(triple.subject)} ${formatValue(triple.predicate)} ${formatValue(triple.object)}.`;
+function toString(triple) {
+	return `${formatValue(triple.subject)} ${formatValue(
+		triple.predicate
+	)} ${formatValue(triple.object)}.`;
 }
 
-function generateQuery(inserts, deletes){
-    let insertString = ""
-    inserts.forEach(insert => {
-        insertString += toString(insert) + "\n";
-    })
-    let deleteString = "";
-    deletes.forEach(deleteOp => {
-        deleteString += toString(deleteOp) + "\n";
-    })
+function generateQuery(inserts, deletes) {
+	let insertString = "";
+	inserts.forEach((insert) => {
+		insertString += toString(insert) + "\n";
+	});
+	let deleteString = "";
+	deletes.forEach((deleteOp) => {
+		deleteString += toString(deleteOp) + "\n";
+	});
 
-    if(insertString){
-        insertString = `INSERT {
+	if (insertString) {
+		insertString = `INSERT {
             GRAPH <${APPLICATION_GRAPH}> {
                 ${insertString}
             }
         }`;
-    }
-    if(deleteString){
-        deleteString = `
+	}
+	if (deleteString) {
+		deleteString = `
         DELETE {
             GRAPH <${APPLICATION_GRAPH}> {
                 ${deleteString}
             }
-        }`
-    }
+        }`;
+	}
 
-    let queryStr = `
+	let queryStr = `
         ${deleteString}\n
         ${insertString}
     `;
-    return queryStr;
+	return queryStr;
 }
 
-app.post('/posts', function(req, res) {
-    let inserts = [];
-    let deletes = [];
-    req.body.forEach(operation => {
-        operation.inserts.forEach(insert => {
-            inserts.push(insert);
-            
-        });
-        operation.deletes.forEach(deleteOp => {
-            deletes.push(deleteOp);
-        })
-    });
+app.post("/posts", function (req, res) {
+	let inserts = [];
+	let deletes = [];
+	req.body.forEach((operation) => {
+		operation.inserts.forEach((insert) => {
+			inserts.push(insert);
+		});
+		operation.deletes.forEach((deleteOp) => {
+			deletes.push(deleteOp);
+		});
+	});
 
-    query(generateQuery(inserts, deletes)).then( function(response) {
-        res.send( JSON.stringify( response ) );
-      })
-      .catch( function(err) {
-        res.send( "Oops something went wrong: " + JSON.stringify( err ) );
-      });
-})
+	query(generateQuery(inserts, deletes))
+		.then(function (response) {
+			res.send(JSON.stringify(response));
+		})
+		.catch(function (err) {
+			res.send("Oops something went wrong: " + JSON.stringify(err));
+		});
+});
 
 app.use(errorHandler);
